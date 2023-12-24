0e1586509eb919d2a936443a5f53e2b497a527d1
sruthilaya <sruthilayagadila@gmail.com>
2023-12-24 19:22:22 +0530

    aws-ssm

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = {
-    { name="roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.devopspractice.store:8080/", type="string" },
+    { name="roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopspractice.store:8080/", type="string" },
     { name="roboshop.dev.frontend.user_url", value="http://user-dev.devopspractice.store:8080/", type="string" },
     { name="roboshop.dev.frontend.cart_url", value="http://cart-dev.devopspractice.store:8080/", type="string" },
     { name="roboshop.dev.frontend.shipping_url", value="http://shipping-dev.devopspractice.store:8080/", type="string" },
