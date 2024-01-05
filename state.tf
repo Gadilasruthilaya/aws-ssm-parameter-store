b3debe85aa5d1ba9efcaa084501b97bc079c633f
sruthilaya <sruthilayagadila@gmail.com>
2024-01-05 16:56:18 +0530

    rabbitmq

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-bucket-sruthi"
-    key    = "dev/terraform.tfvars"
+    key    = "params/terraform.tfvars"
     region = "us-east-1"
   }
 }
