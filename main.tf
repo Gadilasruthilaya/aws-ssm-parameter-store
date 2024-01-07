216acfcc86ab7bd25eb1c2f809e019e5c51d17de
sruthilaya <sruthilayagadila@gmail.com>
2024-01-07 17:03:20 +0530

    mysql

--- main.tf
+++ main.tf
@@ -34,10 +34,12 @@ variable "params" {
     { name = "roboshop.dev.user.MONGO", value =  "true", type =  "String" },
     { name = "roboshop.dev.user.REDIS_HOST", value =  "redis-dev.devopspractice.store", type =  "String" },
     { name = "roboshop.dev.user.MONGO_URL", value =  "mongodb://mongodb-dev.devopspractice.store:27017/users", type =  "String" },
+    { name = "roboshop.dev.rabbitmq.AMQP_USER", value =  "roboshop", type =  "String" },
+    { name = "roboshop.dev.mysql.username", value =  "roboshop", type =  "String" },
 
     #passwords
     { name = "roboshop.dev.payment.AMQP_PASS", value =  "roboshop123", type =  "SecureString" },
-    { name = "roboshop.dev.rabbitmq.AMQP_USER", value =  "roboshop", type =  "String" },
     { name = "roboshop.dev.rabbitmq.AMQP_PASS", value =  "roboshop123", type =  "SecureString" },
+    { name = "roboshop.dev.mysql.password", value =  "roboshop123", type =  "SecureString" },
   ]
 }
